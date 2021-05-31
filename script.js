313e5c9a4fd2da7ff9b976d7a021c5c729aecb06
Astating <cguilb3@gmail.com>
2021-06-01 00:08:55 +0200

    NOT CLEAN, but working with all intended functions :)

--- script.js
+++ script.js
@@ -9,11 +9,15 @@ eraserButton.addEventListener("click", erasing);
 const rainbowButton = document.querySelector("#rainbow");
 rainbowButton.addEventListener("click", rainbowing);
 
+const drknBtn = document.querySelector("#darkening");
+drknBtn.addEventListener("click", darkening);
+
 let gridSquareSize = 16;
 
 function addGrid(gridNum = gridSquareSize) {
   container.textContent = "";
   eraserON = false;
+  darkenON = false;
   container.style.gridTemplateColumns = `repeat(${gridNum}, 1fr)`;
   for (let i = 1; i <= gridNum ** 2; i++) {
     const newGrid = document.createElement("div");
@@ -24,21 +28,36 @@ function addGrid(gridNum = gridSquareSize) {
   const gridSquare = document.querySelectorAll(".grid-square");
   gridSquare.forEach((turtle) => {
     turtle.style.cssText = `background: purple`;
+    turtle.style.opacity = "1";
   });
   gridSquareSize = gridNum;
 }
 
+let darkenON = false;
+
 function changeColor(e) {
   if (eraserON) {
-    e.target.style.background = `purple`;
-  } else {
+    e.target.style.backgroundColor = `purple`;
+    e.target.style.opacity = '1';
+  } else if (
+    darkenON === false ||
+    (darkenON === true && e.target.style.backgroundColor == `purple`)
+  ) {
+    e.target.style.opacity = '1';
     const newColor = Math.floor(Math.random() * 360 + 1);
-    e.target.style.background = `hsl(${newColor}, 100%, 50%)`;
-    console.log(e.target);
+    e.target.style.backgroundColor = `hsl(${newColor}, 100%, 50%)`;
+  } else if (e.target.style.backgroundColor != "purple") {
+    e.target.style.opacity = `${e.target.style.opacity - 0.1}`;
+    if (e.target.style.opacity === "0") {
+      return;
+    }
   }
 }
 
-function darkening() {}
+function darkening() {
+  darkenON = true;
+  eraserON = false;
+}
 
 let eraserON = false;
 
@@ -48,11 +67,14 @@ function erasing() {
 
 function rainbowing() {
   eraserON = false;
+  darkenON = false;
 }
 
 function clearGrid() {
-  let newSize = Number(prompt("What size should it be ? (0-100)", `${gridSquareSize}`));
-  if (newSize <= 100 && newSize >= 0) {
+  let newSize = Number(
+    prompt("What size should it be ? (1-100)", `${gridSquareSize}`)
+  );
+  if (newSize <= 100 && newSize >= 1) {
     addGrid(newSize);
   } else {
     alert("INVALID NUMBER :)");
@@ -60,8 +82,3 @@ function clearGrid() {
 }
 
 addGrid();
-
-const gridSquare = document.querySelectorAll(".grid-square");
-gridSquare.forEach((turtle) => {
-  turtle.style.cssText = `background: purple`;
-});
